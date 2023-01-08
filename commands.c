92ac82aac23d0a17096d83ff986f46754a77271b
André Maree <ammaree@users.noreply.github.com>
2023-01-08 22:48:55 +0200

    Add GAO reporting

--- commands.c
+++ commands.c
@@ -396,6 +396,9 @@ static void vCommandInterpret(int cCmd, bool bEcho) {
 		#if (halSOC_ANA_IN > 0)
 			halGAI_Report();
 		#endif
+		#if (halSOC_ANA_OUT > 0)
+			halGAO_Report();
+		#endif
 		#if (halSOC_DIG_IN > 0)
 			halGDI_Report();
 		#endif
