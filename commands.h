9609d5536ef0287fd20bd77d40d795e914c766df
André Maree <ammaree@users.noreply.github.com>
2024-04-15 22:12:22 +0200

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,30 +1,33 @@
-/*
- * commands.h
- */
+// commands.h
 
 #pragma once
 
 #include <stdarg.h>
+#include "printfx.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // ######################################## General MACROs #########################################
-
-
 // ######################################### enumerations ##########################################
-
-
 // ########################################## structures ###########################################
 
 
+typedef struct __attribute__((packed)) command_t {
+	report_t sRprt;
+	u8_t * pCmd;                    // command string to process
+	int (*Hdlr)(void *);			// pointer to function to empty buffer once processing done
+	void *pVoid;                    // context pointer as parameter 1 for handler
+	bool fEcho;                     // if true all command characters will be echo'd
+	bool fEmpty;
+	bool fFlags;
+} command_t;
+
 // ###################################### Global variables #########################################
-
-
 // ################################### GLOBAL Function Prototypes ##################################
 
-int xCommandProcessString(char *, bool, int (*)(void *, const char *, va_list), void *, const char *, ...);
+int xCommandProcess(command_t * psC);
 
 #ifdef __cplusplus
 }
